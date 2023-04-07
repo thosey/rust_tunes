f369d183ec959dd2252a4d9ef5db3c38766c8e4a
Tim <thosey@Kuki>
2023-04-07 17:07:34 +0100

    Fixed duration bug

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,28 @@ struct Tune {
                                         notes : Vec<Note>,
 }
 
+fn frequency_of_note(pitch: &str, octave: u8) -> f32 {
+  let base_frequency = 440.0; // A4
+  let base_octave = 4;
+  let semitones_from_a = match pitch {
+      "A" => 0,
+      "A#" | "Bb" => 1,
+      "B" => 2,
+      "C" => 3,
+      "C#" | "Db" => 4,
+      "D" => 5,
+      "D#" | "Eb" => 6,
+      "E" => 7,
+      "F" => 8,
+      "F#" | "Gb" => 9,
+      "G" => 10,
+      "G#" | "Ab" => 11,
+      _ => panic!("Invalid pitch"),
+  };
+
+  let semitones_from_base = (octave as i32 - base_octave as i32) * 12 + semitones_from_a;
+  base_frequency * (2f32).powf(semitones_from_base as f32 / 12f32)
+}
 fn main() {
     // Read in first input argument as location
   let arguments: Vec<String> = env::args().collect();
@@ -32,7 +54,7 @@ let location = &arguments[1];
   let tune : Tune = serde_json::from_reader(reader).unwrap();
 
   // Compute the duration of a quarter note in seconds based on the tempo
-  let quarter_note_duration = 60.0 / f32::from(tune.tempo);
+  let quarter_note_duration = 1000.0 * ((60.0 / f32::from(tune.tempo)) as f32);
 
   // Create a Rodio Sink for audio playback
   let(_stream, stream_handle) = OutputStream::try_default().unwrap();
@@ -42,29 +64,19 @@ let location = &arguments[1];
   for
     note in tune.notes {
       // Compute the frequency of the note based on the pitch and octave
-
-    let frequency = match note.pitch.as_str() {
-        "C" => 261.63,
-        "D" => 293.66,
-        "E" => 329.63,
-        "F" => 349.23,
-        "G" => 392.00,
-        "A" => 440.00,
-        "B" => 493.88,
-        _ => 0.0,
-      };
-
+      let frequency = frequency_of_note(&note.pitch, note.octave);
    //output the note to standard output
    //
 println!("Playing note: {:?}", note.pitch);
-
-
       // Compute the duration of the note in seconds
       let duration = (quarter_note_duration * note.duration) as u64;
-      let source = SineWave::new (frequency).take_duration(Duration::from_secs(duration)).amplify(0.20);
+println!("For {:?} seconds", duration);
+      let source = SineWave::new (frequency).take_duration(Duration::from_millis(duration)).amplify(0.20);
       sink.append(source);
     }
 
   // Wait for the audio to finish playing before exiting
+  sink.play();
   sink.sleep_until_end();
 }
+
